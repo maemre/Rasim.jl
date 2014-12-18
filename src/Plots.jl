1053d57a7b2207f343c2677c7e640c0acd17143e
Mehmet Emre <maemre2@gmail.com>
2014-12-18 12:26:25 +0200

    Added legends and now saving temporary figures

--- src/Plots.jl
+++ src/Plots.jl
@@ -5,32 +5,48 @@ using Params
 
 export initplots, plot_ee, displayplots
 
-const colors = ["blue"; "green"; "red"]
+const colors = ["blue"; "green"; "red"; "yellow"; "orange"; "purple"; "black"]
 
-p_ee = None
-p_ee_conv = None
+type PlotFig
+    l :: Array{Any, 1}
+    p :: FramedPlot
+    PlotFig() = new({})
+end
+
+p_ee = PlotFig()
+p_ee_conv = PlotFig()
 
 function initplots()
-    global p_ee = FramedPlot(
+    p_ee.p = FramedPlot(
             title="EE vs Time (Cumulative)",
             xlabel="Time (time slots)",
             ylabel="EE (\\Sigma b/\\Sigma E) (b/J)")
-    global p_ee_conv = FramedPlot(
+    p_ee_conv.p = FramedPlot(
             title="EE vs Time (1201 Pt Average)",
             xlabel="Time (time slots)",
             ylabel="EE (b/E) (b/J)")
 end
 
 function plot_ee(energies, bits, agent, i)
-    add(p_ee, Curve(1:t_total, vec(cumsum(sum(bits, 1), 2) ./ cumsum(sum(energies, 1), 2)), color=colors[i]))
-    add(p_ee_conv, Curve(1:t_total-2399, conv(vec(sum(bits, 1) ./ sum(energies, 1)), fill(1./1201, 1201))[1201:end-1200], color=colors[i]))
+    ee = Curve(1:t_total, vec(cumsum(sum(bits, 1), 2) ./ cumsum(sum(energies, 1), 2)), color=colors[i])
+    setattr(ee, "label", agent)
+    add(p_ee.p, ee)
+    ee_conv = Curve(1:t_total-2399, conv(vec(sum(bits, 1) ./ sum(energies, 1)), fill(1./1201, 1201))[1201:end-1200], color=colors[i])
+    setattr(ee_conv, "label", agent)
+    add(p_ee_conv.p, ee_conv)
+    push!(p_ee.l, ee)
+    push!(p_ee_conv.l, ee_conv)
     nothing
 end
 
 function displayplots()
     for p in [p_ee, p_ee_conv]
-        figure()
-        display(p)
+        f=figure()
+        add(p.p, Legend(.8, .8, p.l))
+        display(p.p)
+        figure(f)
+        savefig(string(getattr(p.p, "title"), ".png"))
+        savefig(string(getattr(p.p, "title"), ".svg"))
     end
 end
 
